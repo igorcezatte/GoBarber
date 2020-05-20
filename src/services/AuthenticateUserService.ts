538236901e084d4d635fc5897b83ae9698a0ca04
Igor Cezatte <igorcezatte13@gmail.com>
2020-05-19 23:52:08 -0300

    generate token in session creation

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -30,8 +32,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email && password combination');
     }
 
+    const token = sign({}, '1e4c1e86e8a3a1dd82c31a243b1dfd01', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
